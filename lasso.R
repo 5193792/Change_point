16afcea8f8e444cceed2b2b1ade85af1a29ee0ba
“HuangXiaogang <5193792@qq.com>
2018-06-05 14:18:17 +0800

    lasso(二次优化方法求解)

--- lasso.R
+++ lasso.R
@@ -45,12 +45,15 @@ x <- as.matrix(w[, 1:10])
 y <- as.matrix(w[, 11])#响应变量
 x2 <- as.matrix(w[, 12:75])
 laa <- lars(x2, y) #lars函数默认方法为lasso
-plot(laa)
 cva <- cv.lars(x2, y, K = 10, plot.it = TRUE)
 best <- cva$index[which.min(cva$cv)]
 coef <- coef.lars(laa, mode = "fraction", s = best)
+#lars解
 coef
-options(digits = 4)
+#lasso解
+lasso(x2,y,t=best*sum(abs(solve(t(x2)%*%x2)%*%t(x2)%*%y)))
+
+#系数轨迹图
 b<- seq(0,1,length.out =100)
 coef1 <- matrix(nrow =100 ,ncol = ncol(x2))
 for (i in 1:100) {
@@ -59,4 +62,4 @@ for (i in 1:100) {
 a <- as.data.frame(cbind(t(coef1),y= 1:ncol(x2)))
 a1 <- melt(a,id.vars = 'y')
 ggplot(a1,aes(variable,value,group=y))+geom_line(aes(col=y))
-
+plot(laa)
